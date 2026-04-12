0c176b3ac7dcf8667af9fb7c4318d384ca9c6f15
skyduyen13726@gmail.com <skyduyen13726@gmail.com>
2026-04-13 01:25:01 +0700

    Duyên thêm func,trig,proc

--- Database/01_tables/identities/employees.sql
+++ Database/01_tables/identities/employees.sql
@@ -15,3 +15,20 @@ CREATE TABLE Employees (
     updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_deleted       NUMBER(1) DEFAULT 0 CHECK (is_deleted IN (0,1))
 );
+
+-- 1. Tạo Sequence cho Nhân viên
+CREATE SEQUENCE seq_employee_code
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
+
+-- 2. Trigger tự động tạo employee_code
+CREATE OR REPLACE TRIGGER trg_GenerateEmployeeCode
+BEFORE INSERT ON Employees
+FOR EACH ROW
+WHEN (NEW.employee_code IS NULL)
+BEGIN
+    :NEW.employee_code := 'NV-' || TO_CHAR(SYSDATE, 'YYYYMMDD') || '-' || LPAD(seq_employee_code.NEXTVAL, 7, '0');
+END;
+/
