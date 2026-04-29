d7ce0af60e6b30a00a311ad8a5b2125fb25de0c6
Thanh Duc <thanhduc1472006@gmail.com>
2026-04-29 08:21:50 +0700

    Eliminate comments in invoice update trigger
    Removed comments that explained the purpose of the update for clarity.

--- Database/05_triggers/ThanhToanTrucTiep/TRG_CapNhatHoaDonKhiKetThucPhien.sql
+++ Database/05_triggers/ThanhToanTrucTiep/TRG_CapNhatHoaDonKhiKetThucPhien.sql
@@ -3,8 +3,6 @@ AFTER UPDATE OF TrangThaiPhien ON PHIENLAMVIEC
 FOR EACH ROW
 WHEN (NEW.TrangThaiPhien = 'Đã kết thúc' AND OLD.TrangThaiPhien != 'Đã kết thúc')
 BEGIN
-    -- Cập nhật lại hóa đơn để trigger TRG_TinhToanHoaDon tính toán lại
-    -- TongTien và ThanhTien dựa trên thời gian kết thúc thực tế
     UPDATE HOADON
     SET NgayLapHoaDon = :NEW.ThoiGianKetThuc
     WHERE MaPhien = :NEW.MaPhien;
