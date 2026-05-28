e8e4d7345cc05595ca097f1538a09cce6c9aaaf6
Bondy <safaridung987@gmail.com>
2026-05-28 23:06:57 +0700

    ThemTraCuuPhieuGiamGiaSQ

--- Database/04_procedures/QuanLyPhieuGiamGia/SP_TraCuuPhieuGiamGia.sql
+++ Database/04_procedures/QuanLyPhieuGiamGia/SP_TraCuuPhieuGiamGia.sql
@@ -40,3 +40,55 @@ EXCEPTION
         RAISE;
 END SP_TraCuuPhieuGiamGia;
 /
+
+DECLARE
+    v_SoPhieu NUMBER;
+    CURSOR c_pgg IS
+        SELECT MaPGG, MaChuSoPGG, GiaTriGiamGia, SLToiDa, TrangThai
+        FROM PHIEUGIAMGIA
+        WHERE TrangThai = 'Đang có hiệu lực'
+        ORDER BY NgayTaoPGG DESC;
+BEGIN
+    SELECT COUNT(*) INTO v_SoPhieu
+    FROM PHIEUGIAMGIA
+    WHERE TrangThai = 'Đang có hiệu lực';
+    DBMS_OUTPUT.PUT_LINE('--- DANH SÁCH PHIẾU GIẢM GIÁ---');
+    DBMS_OUTPUT.PUT_LINE('Số phiếu đang có hiệu lực lần 1: ' || v_SoPhieu);
+    FOR r IN c_pgg LOOP
+       DBMS_OUTPUT.PUT_LINE('- Mã chữ số PGG: ' || r.MaChuSoPGG ||
+                             ' | Giá trị giảm: ' || r.GiaTriGiamGia ||
+                             ' | Số lượng tối đa: ' || r.SLToiDa);
+    END LOOP; -- Kết thúc vòng lặp
+
+    SELECT COUNT(*) INTO v_SoPhieu
+FROM PHIEUGIAMGIA
+WHERE TrangThai = 'Đang có hiệu lực';
+DBMS_OUTPUT.PUT_LINE('--- DANH SÁCH PHIẾU GIẢM GIÁ ---');
+DBMS_OUTPUT.PUT_LINE('Số phiếu đang có hiệu lực : ' || v_SoPhieu);
+FOR r IN c_pgg LOOP
+DBMS_OUTPUT.PUT_LINE('- Mã PGG: ' || r.MaPGG ||
+                         ' | Mã chữ số: ' || r.MaChuSoPGG ||
+                         ' | Trạng thái: ' || r.TrangThai);
+END LOOP;
+END;
+/
+
+DECLARE
+    v_msg VARCHAR2(4000);
+BEGIN
+    SP_ThemPhieuGiamGia(
+        p_MaChuSoPGG          => 'hello', -- Đã sửa dấu nháy đơn tại đây
+        p_GiaTriGiamGia       => 10000,
+        p_GiaTriApDungToiThieu => 50000,
+        p_NgayBatDauApDung    => CURRENT_TIMESTAMP,
+        p_NgayKetThucApDung   => CURRENT_TIMESTAMP + INTERVAL '30' DAY,
+        p_SLToiDa             => 100,
+        p_MaNV                => 'NV0001',
+        p_TrangThai           => 'Đang có hiệu lực',
+        p_outMessage          => v_msg
+    );
+    DBMS_OUTPUT.PUT_LINE(v_msg);
+END;
+/
+
+
